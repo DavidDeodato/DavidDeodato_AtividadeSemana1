1b836f639dcf705a485a7ba9c5954ffcd99fad71
DavidDeodato <david.nascimento@sou.inteli.edu.br>
2024-02-06 23:52:05 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -1,5 +1,8 @@
+//definindo as variaveis das notas
 var nota1 =  8
 var nota2 =  7
+//definindo a variavel media
 var media = (nota1 + nota2)/2
+//estabelecendo condição para ser aprovado ou reprovado
 if (media >= 7 ){console.log ("Aluno Aprovado")}
 else if (media <= 7){console.log ("Aluno Reprovado")}
